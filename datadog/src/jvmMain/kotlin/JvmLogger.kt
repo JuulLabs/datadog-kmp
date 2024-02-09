73ef2c59929d2d5da1b4ff969a686951d9d85b94
Steve Donnelly <sdonn3@gmail.com>
2024-02-09 13:50:30 -0800

    Add Tagging and Attributes functionality, Generic .log() function (#40)
    * Add Tagging and Attributes functionality, Generic .log() function on loggers

--- datadog/src/jvmMain/kotlin/JvmLogger.kt
+++ datadog/src/jvmMain/kotlin/JvmLogger.kt
@@ -1,5 +1,6 @@
 package com.juul.datadog
 
 public interface JvmLogger : Logger {
+    public fun assert(message: String, attributes: Map<String, Any?>? = null, throwable: Throwable? = null)
     public fun verbose(message: String, attributes: Map<String, Any?>? = null, throwable: Throwable? = null)
 }
