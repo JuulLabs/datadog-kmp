8dd880e66c455711a7af57cf13d745fd68a34953
Travis Wyatt <travis.i.wyatt@gmail.com>
2024-06-09 23:23:58 -0700

    Fix compilation errors

--- bundled/src/commonMain/kotlin/DatadogLogger.kt
+++ bundled/src/commonMain/kotlin/DatadogLogger.kt
@@ -6,4 +6,19 @@ public expect class DatadogLogger(
     name: String,
     level: Logger.Level? = null,
     configuration: LoggerConfiguration? = null,
-) : PlatformLogger
+) : PlatformLogger {
+
+    override fun log(
+        level: Logger.Level,
+        message: String,
+        attributes: Map<String, Any?>?,
+        throwable: Throwable?,
+    )
+
+    override fun debug(message: String, attributes: Map<String, Any?>?, throwable: Throwable?)
+    override fun info(message: String, attributes: Map<String, Any?>?, throwable: Throwable?)
+    override fun warn(message: String, attributes: Map<String, Any?>?, throwable: Throwable?)
+    override fun error(message: String, attributes: Map<String, Any?>?, throwable: Throwable?)
+    override fun addAttribute(key: String, value: String)
+    override fun removeAttribute(key: String)
+}
