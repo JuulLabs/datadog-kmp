b1403482a8518c29b7b6f5c22318082963a7857b
Steve Donnelly <steven.donnelly@juul.com>
2025-02-11 10:16:21 -0800

    lint

--- datadog-ktor/src/commonMain/kotlin/LogSubmission.kt
+++ datadog-ktor/src/commonMain/kotlin/LogSubmission.kt
@@ -26,8 +26,7 @@ internal class DatadogLogSubmission(
         http
             .preparePost("api/v2/logs") {
                 setBody(logs)
-            }
-            .executeForResult()
+            }.executeForResult()
             .body<Unit>()
 }
 
