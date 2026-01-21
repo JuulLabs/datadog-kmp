1ccd3471964e25f35f4ab5ba86ff23b1138b88b1
Cedrick Cooke <development@cedrickc.net>
2026-01-21 09:24:37 -0800

    Lint fix

--- datadog/src/wasmJsMain/kotlin/Configuration.wasmJs.kt
+++ datadog/src/wasmJsMain/kotlin/Configuration.wasmJs.kt
@@ -4,5 +4,5 @@ public actual data class Configuration(
     actual val clientToken: String,
     actual val site: Site,
     actual val environment: String,
-    actual val service: String?
+    actual val service: String?,
 )
