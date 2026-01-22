c7815d9849daf9c1f7acfe65bf9bf9c79e302940
Cedrick Cooke <development@cedrickc.net>
2026-01-21 21:21:46 -0800

    Support for uploading over the browser-intake endpoint

--- datadog-ktor/src/commonMain/kotlin/Configuration.kt
+++ datadog-ktor/src/commonMain/kotlin/Configuration.kt
@@ -3,13 +3,23 @@ package com.juul.datadog.ktor
 import com.juul.datadog.Site
 
 public data class Configuration(
-    public val rest: Rest,
+    public val endpoint: Endpoint,
     public val log: Log = Log(),
 ) {
-    public data class Rest(
-        public val apiKey: String,
-        public val site: Site,
-    )
+
+    public sealed interface Endpoint {
+        public val site: Site
+
+        public data class Rest(
+            public override val site: Site,
+            public val apiKey: String,
+        ) : Endpoint
+
+        public data class BrowserIntake(
+            public override val site: Site,
+            public val clientToken: String,
+        ) : Endpoint
+    }
 
     public data class Log(
         public val source: String? = null,
